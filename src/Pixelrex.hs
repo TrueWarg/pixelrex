06a8056e0558c92676c2f62ac9037cf2ec86c90f
TrueWarg <irrmm9000@gmail.com>
2022-09-07 22:34:31 +0200

    Impl DCEL for Voronoi diagram

--- src/Pixelrex.hs
+++ src/Pixelrex.hs
@@ -2,8 +2,10 @@ module Pixelrex
   ( module Pixelrex.Core.Array
   , module Pixelrex.Core.Point
   , module Pixelrex.Segmentation.Slic
+  , module Pixelrex.Generation.Fortune
   ) where
 
 import           Pixelrex.Core.Array
 import           Pixelrex.Core.Point
+import           Pixelrex.Generation.Fortune
 import           Pixelrex.Segmentation.Slic
